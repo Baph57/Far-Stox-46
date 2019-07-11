18fa9fef6a1b6d33a7a3f8692c78fd87e7eee7e2
Baph <Baph@flavortown.yum>
2019-07-11 13:37:26 -0700

    implemented DontDestroyOnLoad for musicplayer

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;//needed for SceneManager.LoadScene(1);
 
 public class MusicPlayer : MonoBehaviour
 {
+    private void Awake()//different Unity Lifecycle method that's called before Start
+    {
+        //lowercase g applies context to attached object
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
