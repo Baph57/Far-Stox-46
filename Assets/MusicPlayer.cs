0de9ed0a79d58b1c57d33db24ba72c1ef3bc3df7
Baph <Baph@flavortown.yum>
2019-07-11 13:29:00 -0700

    been a minute, did a lot of creative work

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//needed for SceneManager.LoadScene(1);
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartGame", 10f);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
