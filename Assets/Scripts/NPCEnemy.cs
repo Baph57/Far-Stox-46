1706e2e302cc672381f101a3007d56f620eed175
Baph <Baph@flavortown.yum>
2019-07-15 16:56:21 -0700

    destroying npc's on particle collision

---
+++ Assets/Scripts/NPCEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("particles collided with enemy " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
