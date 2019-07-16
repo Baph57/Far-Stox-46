fa1fab556a25ff6d9ae69de1c409158bb7e0e44c
Baph <Baph@flavortown.yum>
2019-07-16 13:43:42 -0700

    Using C# to add components to NPCs

--- Assets/Scripts/NPCEnemy.cs
+++ Assets/Scripts/NPCEnemy.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class NPCEnemy : MonoBehaviour
 {
+
+    [SerializeField] GameObject DeathFX;
+    [SerializeField] Transform parent;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
+       //Collider npcBoxCollider = gameObject.AddComponent<BoxCollider>();
+        //npcBoxCollider.isTrigger = true;
+
+        //Ben is not a fan of this, calling it a 'side effect'
+        //I find this to be innacurate and if it doesn't
+        // introduce actual side effects, I'd rather leave it as one line
+        gameObject.AddComponent<BoxCollider>().isTrigger = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private void AddNonTriggerBoxCollider
 
     private void OnParticleCollision(GameObject other)
     {
+        //Quaternion.identity here is simply saying we want no rotation
+        //Or to set the rotation to the current quaterion identity
+        GameObject Explosion = Instantiate(DeathFX, transform.position, Quaternion.identity);
+
+        //this allows us to parent the deathFX to an empty game object
+        Explosion.transform.parent = parent;
+
         print("particles collided with enemy " + gameObject.name);
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //UI namespace, namespaces are collections of classes
+
+public class ScoreBoard : MonoBehaviour
+{
+
+    [SerializeField] int scorePerHit = 50;
+
+    int score; //by deafult ints intialize to zero
+    Text scoreValue; //Text is available by using UE.UI
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreValue = GetComponent<Text>();//reference to component on UI text
+        scoreValue.text = score.ToString();
+    }
+
+    //public means that this function can call methods/funcs outside this class
+
+    public void ScoreHandler()
+    {
+        score = score + scorePerHit;
+        scoreValue.text = score.ToString();
+    }
+}
